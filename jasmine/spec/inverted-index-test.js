b5e6bea5d5caf70bbc854c3852bf786a0e45303d
Mugeri <nyotuo17@gmail.com>
2016-06-07 18:00:42 +0300

    Add first two tests

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,8 +1,10 @@
 
+
 describe("Read book data", function(){
 	it("should read the JSON file and assert its not empty", function(done){
-		readjson().then(function(data){
-			//console.log("in test ", data);
+
+		readJson().then(function(data){
+			console.log("in test ", data);
 			expect(data).toBeTruthy();
 			done();
 		})
@@ -13,10 +15,11 @@ describe("Populate Index", function(){
 	it("should return array of correct indices after search", function(done){
 		console.log('The function is ' + indexCreate());
 		indexCreate().then(function(data){
-		console.log("in test ", data);
-		expect(data).toBe(["Index: 0, title: Alice in Wonderland.", 
-			"Index: 1, title: The Lord of the Rings: The Fellowship of the Ring."]);
-		done();
+			console.log("in test ", data);
+			expect(data).toEqual(["Index: 0, title: Alice in Wonderland.", 
+				"Index: 1, title: The Lord of the Rings: The Fellowship of the Ring."]);
+			expect([1]).toEqual([1]);
+			done();
 		});
 		
 	});
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,41 +1,23 @@
-
-
-function readjson(){
-	return fetch("../jasmine/books.json").then(function(response){
-		return response.json();
-	})
-	.catch(function(err){
-		console.log('There has been a problem with your fetch operation: ' + err);
-		return false;
+function readJson() {
+	return fetch('../jasmine/books.json').
+		then(function(response){
+		return response.json()}).
+		then(function(data){
+			return data;
+		}).catch (function(error){
+			throw error;
 	});
 }
 
 function indexCreate(){
-	var index=[];
-	return fetch('../jasmine/books.json').then(function(response) {
-		console.log('I am def');
-		return response.json();
-	}).catch(function(error) {
-		return 'There was an error'
+	var indarray = [];
+	return readJson().then(function(response){
+		var data = response;
+		data.map(function(element, index){
+			indarray.push('Index: ' + index + ', title: ' + element.title);
+
+		});
+		return indarray;
+		
 	});
-	// fetch("../jasmine/books.json").then(function(response){
-	// 	response.json().then(function(data){
-	// 	data.map(function(x, y){
-	// 		//console.log("Index: "+ y +", title: " + x.title);
-	// 		index.push("Index: "+ y +", title: " + x.title);
-
-	// 	});
-	// 	console.log(index);
-	// 	return index;
-		
-	// });
-		
-
-	// })
-	// .catch(function(err){
-	// 	console.log('There has been a problem with your fetch operation: ' + err);
-	// 	return false;
-	// });
-    // return 'abc'
-	
 }
