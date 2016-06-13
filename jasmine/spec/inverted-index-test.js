a7098d7648befa9c838aa74c22ea698b84143f93
andela-omugeri <olive.nyotu@andela.com>
2016-06-13 17:11:15 +0300

    split content to words

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -14,11 +14,19 @@ describe("Read book data", function(){
 describe("Populate Index", function(){
 	it("should return array of correct indices after search", function(done){
 		indexCreate().then(function(data){
-			expect(data).toEqual(["Index: 0, title: Alice in Wonderland.", 
-				"Index: 1, title: The Lord of the Rings: The Fellowship of the Ring."]);
-			expect([1]).toEqual([1]);
+			expect(data).toEqual([ [ [ 'Alice', 'in', 'Wonderland.' ], 0 ], [ [ 'Alice', 'falls', 'into', 'a', 'rabbit', 'hole', 'and', 'enters', 'a', 'world', 'full', 'of', 'imagination.' ], 0 ], [ [ 'The', 'Lord', 'of', 'the', 'Rings:', 'The', 'Fellowship', 'of', 'the', 'Ring.' ], 1 ], [ [ 'An', 'unusual', 'alliance', 'of', 'man,', 'elf,', 'dwarf,', 'wizard', 'and', 'hobbit', 'seek', 'to', 'destroy', 'a', 'powerful', 'ring.' ], 1 ] ]);
 			done();
 		});
-		
+
+	});
+});
+
+describe("Verify Index", function(){
+	it("verifies that the index maps correctly", function(done){
+
+		getIndex('Alice').then(function(data){
+			expect(data).toEqual([0]);
+			done();
+		});
 	});
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -11,14 +11,52 @@ function readJson() {
 
 function indexCreate(){
 	var indarray = [];
+	var bookarray = [];
+	var sentenceArray = [];
+	var wordArray = [];
 	return readJson().then(function(response){
 		var data = response;
 		data.map(function(element, index){
+			var content = [];
 			var titles = Object.keys(element);//[title,text]
+			var i=0;
 			indarray.push('Index: ' + index + ', title: ' + element[titles[0]]);
 
+			for (var i = 0; i < titles.length; i++) {
+				content.push(element[titles[i]]);
+			}
+
+			content.map(function(phrase, indexOfPhrase) {
+					bookarray.push([phrase.split(' '), index]);
+			});
 		});
-		return indarray;
-		
+
+	 	return bookarray;
+ });
+}
+
+function getIndex(word){
+	return indexCreate().then(function(response){
+		var index = [];
+		var i=0, j=0, count=0;
+		for(i=0; i<response.length; i++){//loop through the index
+			sentence = response[i][0];
+			 console.log(response);
+			for(j=0; j<sentence.length; j++) {
+				// console.log('word is: ', word);
+				// console.log('word being read is: ', String(sentence[j].split(',')));
+				while (count==0){
+					if( word == sentence[j].split(',')){
+						index.push(response[i][1]);
+						count ++;
+					}
+				}
+
+			}
+
+		}
+		return index;
+
 	});
+
 }
