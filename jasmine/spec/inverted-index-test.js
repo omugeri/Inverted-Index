9e529397f2afecc661e3f9a3547754f810fcdb4b
andela-omugeri <olive.nyotu@andela.com>
2016-06-19 00:34:09 +0300

    Allow searchIndex to look up array of words

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,37 +1,51 @@
 
 var index = new Index();
+var type = '';
 describe("Read book data", function(){
 	it("should read the JSON file and assert its not empty", function(done){
-
-		index.readJson().then(function(data){
-			console.log("in test ", data);
+		index.readJson('../jasmine/books.json').then(function(data){
 			expect(data).toBeTruthy();
 			done();
-		})
+		});
+	});
+	it("confirms that each object contains property whose value is a string", function(done){
+		index.readJson('../jasmine/books.json').then(function(data) {
+			var type = false;
+			keys = Object.keys(data[1]);
+			expect(typeof data[1][keys[0]]).toBe(typeof 'example');
+			done();
+		});
 	});
 });
 
 describe("Populate Index", function(){
-	it("should return array of correct indices after search", function(done){
-		index.createIndex().then(function(data){
+	it("verifies that the index is created", function(done){
+		index.createIndex('../jasmine/books.json').then(function(data){
 			expect(data).toEqual(jasmine.any(Object));
 			done();
 		});
-
 	});
+
+	it('verifies that the index is correct', function(done){
+		index.createIndex('../jasmine/books.json').then(function(data){
+			expect(data['alice']).toEqual([0]);
+			done();
+		})
+	})
 });
 
-describe("Verify Index", function(){
-	it("verifies that the right index of a word is given", function(done){
-				index.searchIndex('and').then(function(data){
-						expect(data).toEqual([0, 1]);
+describe("Search Index", function(){
+	it("verifies that the correct results of a word are given", function(done){
+						expect(index.searchIndex('and')).toEqual([[0, 1]]);
 						done();
-				});
 	});
-	it("verifies that it responds if word is not found", function(done){
-		index.searchIndex('Olive').then(function(data){
-			expect(data).toEqual('Word not found!');
-			done();
-		});
+	it("verifies that it responds if word is not found", function(){
+			expect(index.searchIndex('Olive')).toEqual('Word not found!');
+	});
+	it("verifies that it can handle multiple search terms", function(){
+			expect(index.searchIndex('Alice and Wonderland')). toEqual([[0],[0,1], [0]]);
+	});
+	it("verifies that it can handle an array search terms", function(){
+			expect(index.searchIndex(['Alice', 'Rings'])). toEqual([[0],[1]]);
 	});
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,69 +1,105 @@
 class Index {
-	readJson() {
-			return fetch('../jasmine/books.json').
-				then(function(response){
-				return response.json()}).
-				then(function(data){
-					return data;
-				}).catch (function(error){
+
+	constructor(){
+		this.invertedIndex = {};
+	}
+
+	readJson( url ) {
+			return fetch( url ).//pick data from url
+				then(function( response ){
+				return response.json()
+			}).//turn data into json
+				then(function( data ){
+					return data;							//give results as a promise
+				}).catch (function( error ){
 					throw error;
 			});
 		}
 
-	createIndex(){
-		var bookarray = {};
+	createIndex( url ){
 		var sentence = [];
-		return this.readJson().then(function(response) {
+		var self = this;
+
+		return this.readJson( url ).then(function( response ) {
 			var data = response;
-			data.map(function(element, index) {
+			data.map(function( element, index ) {//get the documents in the json object
 				var content = [];
-				var titles = Object.keys(element);//[title,text]
+				var titles = Object.keys( element );//get the keys in each object of the json file
 				var i=0;
 
 				for (i = 0; i < titles.length; i++) {
-					content.push(element[titles[i]]);
+					content.push( element[titles[i]] );	//store the content of each object.
 				}
 
-				content.map(function(phrase, indexOfPhrase) {
+				content.map(function( phrase, indexOfPhrase ) {
 					//replace all special characters with space and change to lowercase
-					sentence = phrase.toLowerCase().replace(/\W/g, ' ').replace(/\s+/g, ' ').trim();
+					sentence = phrase.toLowerCase().replace( /\W/g, ' ' ).replace( /\s+/g, ' ' ).trim();
 					var words = sentence.split(' ');//split the sentence into words
-					for (var i=0; i<words.length; i++) {
-						if(bookarray[words[i]]) {
-							if(index !== bookarray[words[i]].value) {
-								bookarray[words[i]].push(index);
+					for ( var i=0; i<words.length; i++ ) {
+						if( self.invertedIndex[words[i]] ) {//check if the word exists in the index
+							if( index !== self.invertedIndex[words[i]].value ) {
+								self.invertedIndex[words[i]].push( index );//add another value in the index
 							}
 						}
 						else {
-							bookarray[words[i]] = [index];
+							self.invertedIndex[words[i]] = [index];//add a new entry in the index
 						}
 					}
 					// make the elements in the array unique
-					for(var j=0; j<words.length; j++) {
-						var set = new Set(bookarray[words[j]]);
-						bookarray[words[j]] = Array.from(set);
+					for( var j=0; j<words.length; j++ ) {
+						var set = new Set( self.invertedIndex[words[j]] );
+						self.invertedIndex[words[j]] = Array.from( set );
 					}
 				});
 			});
-			return bookarray;
+			return self.invertedIndex;
 	 });
 	}
-	getIndex(url){
-		this.createIndex().then(function(response){
-			var index = console.log(response);
-		})
+	getIndex( title ) {//get the whole index.
+		return self.invertedIndex;
 	}
 
-	searchIndex(word){
-		return this.createIndex().then(function(response) {
-			if(response[word] === undefined){
-				return 'Word not found!';
+	searchIndex( query ) {//look for a specific entry in the invertedIndex
+		var index = [];
+		var answer = [];
+		if( Array.isArray( query ) ) { //check if the query is an array.
+			for( var i=0; i<query.length; i++ ) {//loop through the array
+				var word = query[i].toLowerCase(); //change the word to lowercase
+				if(this.invertedIndex[word] === undefined){
+					return 'Word not found!';
+				}
+				else {
+					answer.push( this.invertedIndex[word] );
+				}
+				index = answer;
 			}
-			else {
-				var index = response[word];
-				return index;
+			return index;
+		}
+		else {
+			var words = query.toLowerCase().replace( /\W/g, ' ' ).split(' ');//remove special characters and split if its a sentence
+			for( var i=0; i<words.length; i++ ) {//loop through the sentence
+				// answer = findIndex( words[i] );	//each word to the findIndex method
+				if(this.invertedIndex[words[i]] === undefined){
+					return 'Word not found!';
+				}
+				else {
+					answer.push( this.invertedIndex[words[i]] );
+				}
+				index = answer;
+			}
+			return index;	//return the results
+		}
+	}
 
-			}
-		});
+	findIndex( term ) {	//checks for the index of the term given.
+		term = term.toLowerCase();
+		var index = [];
+		if( this.invertedIndex[term] === undefined ) {
+			return 'Word not found!';
+		}
+		else {
+			index.push( this.invertedIndex[term] );
+			return index;
+		}
 	}
 }
